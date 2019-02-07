139aeae8c0035f9dd3909e4f030520a732d25932
Alan Houston <alanhouston5000@gmail.com>
2019-02-07 09:44:45 -0600

    aca-database initial

--- database.js
+++ database.js
@@ -1,9 +1,15 @@
 let fs = require("fs");
 
-exports.create = function(type,data,doneCallback){
-  
+exports.create = function(type,data,doneCallback){  
+    
 }
 exports.find = function(type,id,doneCallback){
-
+    //type = "user"
+    fs.readFile("db.json", (err,data) => {
+        let parsedData = JSON.parse(data);
+        let users = parsedData(type);
+        let user = users.find(u=>user.id == id)
+        doneCallback(user);
+    });
 }
 
--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+// Intro
+// We need to build a web server that is capable of saving things forever. Use the file system to save data from http messages in json format. Let's code our own database in database.js. Look at db.json and understand the json structure. Remember how to use node file system.
+
+// Find
+// Use fs readFile to get the contents of db.json.
+// Use JSON.parse to turn the string contents into an object
+// Pay attention to what this object looks like
+// Use the type argument to get the proper key/value which is an array
+// Find the object from the array that has the id value equal to the id argument
+// Call the doneCallback function passing in the object you found.
+// Create
+// Use fs readFile to get the contents of db.json.
+// Use JSON.parse to turn the string contents into an object
+// Pay attention to what this object looks like
+// Use the type argument to get the proper key/value which is an array
+// Push the data to the array
+// Use JSON.stringify to turn the object back into a string
+// Use fs writeFile to save the string back into the file db.json.
+// Call the doneCallback function, passing in data
 
 var http = require('http');
 let server = http.createServer(messageReceived);
 server.listen(8080);
+let {find} = require("./database");
 
 
 function messageReceived(req, res) {
@@ -13,7 +33,10 @@ function messageReceived(req, res) {
     else if(req.method === "GET" && req.url.indexOf("/users/") > -1){
       let id = req.url.split("/");
       id = Number(id[2]);
-      res.end();
+      find("users",id,(user)=>{
+        res.write(JSON.stringify(user));
+        res.end();
+      })
     }
     else if(req.method === "POST" && req.url === "/users"){
         let body = [];
